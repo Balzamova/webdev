8d149dd670d7c4912e7a2296247a76c70bb9eb4c
Balzamova <balzam0401@gmail.com>
2021-02-04 16:20:50 +0300

    feat: add media properties for 3-4 pages; fix: changed tree in header

--- webdev-first/script.js
+++ webdev-first/script.js
@@ -1,7 +1,14 @@
 // Burger Click
 document.querySelector('.header__burger').onclick = function () {
     document.querySelector('.header__burger').classList.toggle('active');
-    document.querySelector('.header__menu').classList.toggle('active');
+    document.querySelector('.header__img').classList.toggle('active');
+
+
+    
+
+
+    document.querySelector('.header__menu-left').classList.toggle('active');
+    document.querySelector('.header__menu-right').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock');
   }
 
