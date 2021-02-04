56ee850dc7291cc94b1df7bc969b3bbe9d8cf58f
Balzamova <balzam0401@gmail.com>
2021-02-04 21:40:03 +0300

    fix: fixed burgers errors

--- webdev-first/script.js
+++ webdev-first/script.js
@@ -1,14 +1,9 @@
 // Burger Click
 document.querySelector('.header__burger').onclick = function () {
     document.querySelector('.header__burger').classList.toggle('active');
-    document.querySelector('.header__img').classList.toggle('active');
-
-
+    document.querySelector('.header__body').classList.toggle('active');
+    document.querySelector('.header__img img').classList.toggle('active');
     
-
-
-    document.querySelector('.header__menu-left').classList.toggle('active');
-    document.querySelector('.header__menu-right').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock');
   }
 
