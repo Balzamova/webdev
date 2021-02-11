ff9e31053a9f2d5f71735afab6feab6279d59662
Balzamova <balzam0401@gmail.com>
2021-02-11 20:00:42 +0300

    fix: fixed logo and burger errors

--- webdev-first/script.js
+++ webdev-first/script.js
@@ -2,7 +2,6 @@
 document.querySelector('.header__burger').onclick = function () {
     document.querySelector('.header__burger').classList.toggle('active');
     document.querySelector('.header__body').classList.toggle('active');
-    document.querySelector('.header__img img').classList.toggle('active');
     
     document.querySelector('body').classList.toggle('lock');
   }
